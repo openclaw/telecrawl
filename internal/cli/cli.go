openclaw/wacrawl

feat: expose contact export command

internal/cli/cli.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"text/tabwriter"
 	"time"
+	"unicode"
 
 	"github.com/steipete/wacrawl/internal/backup"
 	"github.com/steipete/wacrawl/internal/store"
@@ -97,6 +98,8 @@ func Run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 		return a.runStatus(ctx, rest[1:])
 	case "chats":
 		return a.runChats(ctx, rest[1:])
+	case "contacts":
+		return a.runContacts(ctx, rest[1:])
 	case "unread":
 		return a.runUnread(ctx, rest[1:])
 	case "messages":
@@ -198,6 +201,107 @@ func (a *app) runImport(ctx context.Context, command string, args []string) erro
 		}
 		return a.print(stats)
 	})
+}
+
+type contactExport struct {
+	Contacts []exportedContact `json:"contacts"`
+}
+
+type exportedContact struct {
+	DisplayName  string   `json:"display_name"`
+	PhoneNumbers []string `json:"phone_numbers"`
+}
+
+func (a *app) runContacts(ctx context.Context, args []string) error {
+	if len(args) == 0 || args[0] != "export" {
+		return usageErr(errors.New("contacts supports export only"))
+	}
+	fs := flag.NewFlagSet("contacts export", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+	if err := fs.Parse(args[1:]); err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			printCommandUsage(a.stdout, "contacts", "export")
+			return nil
+		}
+		return usageErr(err)
+	}
+	if fs.NArg() != 0 {
+		return usageErr(errors.New("contacts export takes no arguments"))
+	}
+	return a.withStore(ctx, func(st *store.Store) error {
+		contacts, err := st.Contacts(ctx)
+		if err != nil {
+			return err
+		}
+		return a.print(contactExport{Contacts: exportContacts(contacts)})
+	})
+}
+
+func exportContacts(contacts []store.Contact) []exportedContact {
+	out := make([]exportedContact, 0, len(contacts))
+	for _, contact := range contacts {
+		name := contactDisplayName(contact)
+		phone := strings.TrimSpace(contact.Phone)
+		if name == "" || phone == "" {
+			continue
+		}
+		out = append(out, exportedContact{DisplayName: name, PhoneNumbers: []string{phone}})
+	}
+	return out
+}
+
+func contactDisplayName(contact store.Contact) string {
+	for _, name := range []string{
+		contact.FullName,
+		contact.BusinessName,
+		strings.TrimSpace(contact.FirstName + " " + contact.LastName),
+	} {
+		if cleaned := cleanContactName(name, contact); cleaned != "" {
+			return cleaned
+		}
+	}
+	return ""
+}
+
+func cleanContactName(name string, contact store.Contact) string {
+	name = strings.TrimSpace(name)
+	switch {
+	case name == "":
+		return ""
+	case name == strings.TrimSpace(contact.Phone):
+		return ""
+	case name == strings.TrimSpace(contact.JID):
+		return ""
+	case name == strings.TrimSpace(contact.Username):
+		return ""
+	case name == strings.TrimSpace(contact.LID):
+		return ""
+	case strings.HasPrefix(name, "@"):
+		return ""
+	case looksLikePhone(name):
+		return ""
+	default:
+		return name
+	}
+}
+
+func looksLikePhone(value string) bool {
+	value = strings.TrimSpace(value)
+	if value == "" {
+		return false
+	}
+	digits := 0
+	other := 0
+	for _, r := range value {
+		switch {
+		case unicode.IsDigit(r):
+			digits++
+		case strings.ContainsRune(" +()-.", r):
+		default:
+			other++
+		}
+	}
+	return digits >= 5 && other == 0
 }
 
 func (a *app) runChats(ctx context.Context, args []string) error {
@@ -477,6 +581,7 @@ Commands:
   sync        Alias for import.
   status      Show archive status.
   chats       List chats.
+  contacts    Export archived contacts for clawdex.
   unread      List chats with unread messages.
   messages    List archived messages.
   search      Search archived messages.
@@ -497,6 +602,7 @@ Examples:
   wacrawl doctor
   wacrawl sync
   wacrawl unread --limit 20
+  wacrawl --json --sync never contacts export
   wacrawl --json search "invoice" --from-them --after 2026-01-01
   wacrawl help messages
 `)
@@ -567,6 +673,15 @@ Examples:
   wacrawl chats --limit 20
   wacrawl chats --unread
   wacrawl --json chats --limit 100
+`)
+	case "contacts", "contacts export":
+		_, _ = fmt.Fprint(w, `Export archived contacts for clawdex.
+
+Usage:
+  wacrawl --json --sync never contacts export
+
+Examples:
+  wacrawl --json --sync never contacts export
 `)
 	case "unread":
 		_, _ = fmt.Fprint(w, `List chats with unread messages.

internal/cli/cli_test.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"flag"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -37,6 +39,7 @@ func TestRunEndToEnd(t *testing.T) {
 		{"import copy media", []string{"--db", dbPath, "--source", source, "import", "--copy-media"}, "media_copied=1"},
 		{"status", []string{"--db", dbPath, "status"}, "unread_messages=2"},
 		{"chats", []string{"--db", dbPath, "chats", "--limit", "5"}, "UNREAD"},
+		{"contacts export", []string{"--db", dbPath, "--json", "--sync", "never", "contacts", "export"}, `"display_name": "Alice Contact"`},
 		{"chats unread", []string{"--db", dbPath, "chats", "--unread", "--limit", "5"}, "Launch Group"},
 		{"unread", []string{"--db", dbPath, "unread", "--limit", "5"}, "Launch Group"},
 		{"messages", []string{"--db", dbPath, "messages", "--chat", "123@g.us", "--asc"}, "launch now"},
@@ -53,6 +56,101 @@ func TestRunEndToEnd(t *testing.T) {
 				t.Fatalf("stdout missing %q:\n%s", tc.want, stdout.String())
 			}
 		})
+	}
+}
+
+func TestContactsExportUsesContractShapeAndSkipsUnsafeNames(t *testing.T) {
+	ctx := context.Background()
+	dbPath := filepath.Join(t.TempDir(), "archive.db")
+	st, err := store.Open(ctx, dbPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = st.Close() }()
+	contacts := []store.Contact{
+		{JID: "safe@s.whatsapp.net", Phone: "+15550100", FullName: "Safe Person"},
+		{JID: "business@s.whatsapp.net", Phone: "+15550101", BusinessName: "Business Name"},
+		{JID: "first-last@s.whatsapp.net", Phone: "+15550102", FirstName: "First", LastName: "Last"},
+		{JID: "username@s.whatsapp.net", Phone: "+15550103", Username: "handle", FullName: "@handle"},
+		{JID: "phone@s.whatsapp.net", Phone: "+15550104", FullName: "+15550104"},
+		{JID: "jid@s.whatsapp.net", Phone: "+15550105", FullName: "jid@s.whatsapp.net"},
+		{JID: "blank@s.whatsapp.net", Phone: "+15550106"},
+		{JID: "missing-phone@s.whatsapp.net", FullName: "Missing Phone"},
+	}
+	if err := st.ReplaceAll(ctx, store.ImportStats{}, contacts, nil, nil, nil, nil); err != nil {
+		t.Fatal(err)
+	}
+	var stdout, stderr bytes.Buffer
+	if err := Run(ctx, []string{"--db", dbPath, "--json", "--sync", "always", "contacts", "export"}, &stdout, &stderr); err != nil {
+		t.Fatalf("contacts export: %v stderr=%s", err, stderr.String())
+	}
+	var payload struct {
+		Contacts []struct {
+			DisplayName  string   `json:"display_name"`
+			PhoneNumbers []string `json:"phone_numbers"`
+			JID          string   `json:"jid"`
+			Username     string   `json:"username"`
+		} `json:"contacts"`
+	}
+	if err := json.Unmarshal(stdout.Bytes(), &payload); err != nil {
+		t.Fatalf("json = %s err=%v", stdout.String(), err)
+	}
+	assertContactExportKeys(t, stdout.Bytes())
+	gotNames := make([]string, 0, len(payload.Contacts))
+	for _, contact := range payload.Contacts {
+		gotNames = append(gotNames, contact.DisplayName)
+		if contact.JID != "" || contact.Username != "" {
+			t.Fatalf("leaked source fields = %#v", contact)
+		}
+		if len(contact.PhoneNumbers) != 1 {
+			t.Fatalf("bad phone numbers = %#v", contact)
+		}
+	}
+	wantNames := []string{"Business Name", "First Last", "Safe Person"}
+	if !reflect.DeepEqual(gotNames, wantNames) {
+		t.Fatalf("names = %#v, want %#v", gotNames, wantNames)
+	}
+}
+
+func assertContactExportKeys(t *testing.T, data []byte) {
+	t.Helper()
+	var root map[string]json.RawMessage
+	if err := json.Unmarshal(data, &root); err != nil {
+		t.Fatal(err)
+	}
+	contactsJSON, ok := root["contacts"]
+	if !ok || len(root) != 1 {
+		t.Fatalf("root keys = %#v, want only contacts", root)
+	}
+	var contacts []map[string]json.RawMessage
+	if err := json.Unmarshal(contactsJSON, &contacts); err != nil {
+		t.Fatal(err)
+	}
+	for _, contact := range contacts {
+		if _, ok := contact["display_name"]; !ok {
+			t.Fatalf("contact keys = %#v, missing display_name", contact)
+		}
+		if _, ok := contact["phone_numbers"]; !ok {
+			t.Fatalf("contact keys = %#v, missing phone_numbers", contact)
+		}
+		if len(contact) != 2 {
+			t.Fatalf("contact keys = %#v, want only display_name and phone_numbers", contact)
+		}
+	}
+}
+
+func TestMetadataAdvertisesContactExport(t *testing.T) {
+	manifest := controlManifest()
+	command, ok := manifest.Commands["contact-export"]
+	if !ok {
+		t.Fatalf("commands = %#v", manifest.Commands)
+	}
+	if command.Mutates || !command.JSON {
+		t.Fatalf("contact-export command = %#v", command)
+	}
+	want := []string{"wacrawl", "--json", "--sync", "never", "contacts", "export"}
+	if !reflect.DeepEqual(command.Argv, want) {
+		t.Fatalf("argv = %#v, want %#v", command.Argv, want)
 	}
 }
 

internal/cli/control.go
@@ -19,10 +19,11 @@ func controlManifest() control.Manifest {
 	m.Capabilities = []string{"metadata", "doctor", "status", "sync", "search", "backup"}
 	m.Privacy = control.Privacy{ContainsPrivateMessages: true, ExportsSecrets: false, LocalOnlyScopes: []string{"whatsapp-desktop", "sqlite", "encrypted-git-backup"}}
 	m.Commands = map[string]control.Command{
-		"doctor": {Title: "Doctor", Argv: []string{"wacrawl", "--json", "doctor"}, JSON: true},
-		"status": {Title: "Status", Argv: []string{"wacrawl", "--json", "--sync", "never", "status"}, JSON: true},
-		"sync":   {Title: "Sync", Argv: []string{"wacrawl", "--json", "sync"}, JSON: true, Mutates: true},
-		"search": {Title: "Search", Argv: []string{"wacrawl", "--json", "--sync", "auto", "search"}, JSON: true},
+		"doctor":         {Title: "Doctor", Argv: []string{"wacrawl", "--json", "doctor"}, JSON: true},
+		"status":         {Title: "Status", Argv: []string{"wacrawl", "--json", "--sync", "never", "status"}, JSON: true},
+		"sync":           {Title: "Sync", Argv: []string{"wacrawl", "--json", "sync"}, JSON: true, Mutates: true},
+		"search":         {Title: "Search", Argv: []string{"wacrawl", "--json", "--sync", "auto", "search"}, JSON: true},
+		"contact-export": {Title: "Export contacts", Argv: []string{"wacrawl", "--json", "--sync", "never", "contacts", "export"}, JSON: true},
 	}
 	return m
 }

internal/store/export.go
@@ -64,6 +64,10 @@ func (s *Store) ExportAll(ctx context.Context) (SnapshotData, error) {
 	return SnapshotData{Contacts: contacts, Chats: chats, Groups: groups, Participants: participants, Messages: messages}, nil
 }
 
+func (s *Store) Contacts(ctx context.Context) ([]Contact, error) {
+	return s.exportContacts(ctx)
+}
+
 func (s *Store) ImportSnapshot(ctx context.Context, data SnapshotData, sourcePath string, finishedAt time.Time) error {
 	return s.ReplaceAll(ctx, data.ImportStats(sourcePath, s.Path(), finishedAt), data.Contacts, data.Chats, data.Groups, data.Participants, data.Messages)
 }

openclaw/telecrawl

internal/cli/cli.go
@@ -13,6 +13,7 @@ import (
 	"sort"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/openclaw/telecrawl/internal/backup"
 	"github.com/openclaw/telecrawl/internal/store"
@@ -466,6 +467,9 @@ func (r *runtime) runFolders(args []string) error {
 }
 
 func (r *runtime) runContacts(args []string) error {
+	if len(args) > 0 && args[0] == "export" {
+		return r.runContactsExport(args[1:])
+	}
 	fs := flag.NewFlagSet("telecrawl contacts", flag.ContinueOnError)
 	fs.SetOutput(io.Discard)
 	limit := fs.Int("limit", 100, "")
@@ -482,6 +486,94 @@ func (r *runtime) runContacts(args []string) error {
 		}
 		return r.print(contacts)
 	})
+}
+
+type contactExport struct {
+	Contacts []exportedContact `json:"contacts"`
+}
+
+type exportedContact struct {
+	DisplayName  string   `json:"display_name"`
+	PhoneNumbers []string `json:"phone_numbers"`
+}
+
+func (r *runtime) runContactsExport(args []string) error {
+	fs := flag.NewFlagSet("telecrawl contacts export", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+	if err := fs.Parse(args); err != nil {
+		return usageErr(err)
+	}
+	if fs.NArg() != 0 {
+		return usageErr(errors.New("contacts export takes no arguments"))
+	}
+	return r.withStore(func(st *store.Store) error {
+		contacts, err := st.ExportContacts(r.ctx)
+		if err != nil {
+			return err
+		}
+		return r.print(contactExport{Contacts: exportContacts(contacts)})
+	})
+}
+
+func exportContacts(contacts []store.Contact) []exportedContact {
+	out := make([]exportedContact, 0, len(contacts))
+	for _, contact := range contacts {
+		name := contactDisplayName(contact)
+		phone := strings.TrimSpace(contact.Phone)
+		if name == "" || phone == "" {
+			continue
+		}
+		out = append(out, exportedContact{DisplayName: name, PhoneNumbers: []string{phone}})
+	}
+	return out
+}
+
+func contactDisplayName(contact store.Contact) string {
+	if name := cleanContactName(contact.FullName, contact); name != "" {
+		return name
+	}
+	return cleanContactName(strings.TrimSpace(contact.FirstName+" "+contact.LastName), contact)
+}
+
+func cleanContactName(name string, contact store.Contact) string {
+	name = strings.TrimSpace(name)
+	switch {
+	case name == "":
+		return ""
+	case name == strings.TrimSpace(contact.Phone):
+		return ""
+	case name == strings.TrimSpace(contact.JID):
+		return ""
+	case name == strings.TrimSpace(contact.Username):
+		return ""
+	case name == strings.TrimSpace(contact.LID):
+		return ""
+	case strings.HasPrefix(name, "@"):
+		return ""
+	case looksLikePhone(name):
+		return ""
+	default:
+		return name
+	}
+}
+
+func looksLikePhone(value string) bool {
+	value = strings.TrimSpace(value)
+	if value == "" {
+		return false
+	}
+	digits := 0
+	other := 0
+	for _, r := range value {
+		switch {
+		case unicode.IsDigit(r):
+			digits++
+		case strings.ContainsRune(" +()-.", r):
+		default:
+			other++
+		}
+	}
+	return digits >= 5 && other == 0
 }
 
 func (r *runtime) runTopics(args []string) error {
@@ -810,6 +902,7 @@ usage:
   telecrawl [--json] status
   telecrawl [--json] folders
   telecrawl [--json] contacts [--limit N]
+  telecrawl --json contacts export
   telecrawl [--json] chats [--limit N] [--unread] [--folder ID]
   telecrawl [--json] topics --chat ID [--limit N]
   telecrawl [--json] messages [--chat ID] [--topic ID] [--limit N] [--after DATE]

internal/cli/cli_test.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"os"
 	"path/filepath"
 	"slices"
@@ -74,6 +75,115 @@ func TestImportResultForChatFiltersContacts(t *testing.T) {
 	want := []string{"111", "10"}
 	if !slices.Equal(got, want) {
 		t.Fatalf("contacts = %v, want %v", got, want)
+	}
+}
+
+func TestContactsExportUsesContractShapeAndSkipsUnsafeNames(t *testing.T) {
+	ctx := context.Background()
+	db := filepath.Join(t.TempDir(), "telecrawl.db")
+	st, err := store.Open(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = st.Close() }()
+	contacts := make([]store.Contact, 0, 104)
+	for i := 0; i < 101; i++ {
+		contacts = append(contacts, store.Contact{
+			JID:      "safe-" + string(rune('a'+(i%26))) + "-" + string(rune('a'+((i/26)%26))),
+			Phone:    "+1555010" + strings.Repeat("0", 3-len(string(rune('0'+(i%10))))) + string(rune('0'+(i%10))),
+			FullName: "Safe Person",
+		})
+	}
+	contacts = append(contacts,
+		store.Contact{JID: "first-last", Phone: "+15559990001", FirstName: "First", LastName: "Last"},
+		store.Contact{JID: "username-only", Phone: "+15559990002", Username: "handle", FullName: "@handle"},
+		store.Contact{JID: "phone-only", Phone: "+15559990003", FullName: "+15559990003"},
+		store.Contact{JID: "jid-only", Phone: "+15559990004", FullName: "jid-only"},
+		store.Contact{JID: "blank-name", Phone: "+15559990005"},
+		store.Contact{JID: "no-phone", FullName: "No Phone"},
+	)
+	if err := st.ReplaceAll(ctx, store.ImportStats{}, contacts, nil, nil, nil, nil, nil); err != nil {
+		t.Fatal(err)
+	}
+	var out, errOut bytes.Buffer
+	err = Run(ctx, []string{"--json", "--db", db, "contacts", "export"}, &out, &errOut)
+	if err != nil {
+		t.Fatalf("contacts export: %v stderr=%s", err, errOut.String())
+	}
+	var payload struct {
+		Contacts []struct {
+			DisplayName  string   `json:"display_name"`
+			PhoneNumbers []string `json:"phone_numbers"`
+			JID          string   `json:"jid"`
+			Username     string   `json:"username"`
+		} `json:"contacts"`
+	}
+	if err := json.Unmarshal(out.Bytes(), &payload); err != nil {
+		t.Fatalf("json = %s err=%v", out.String(), err)
+	}
+	assertContactExportKeys(t, out.Bytes())
+	if len(payload.Contacts) != 102 {
+		t.Fatalf("contacts = %d, want 102", len(payload.Contacts))
+	}
+	var sawFirstLast bool
+	for _, contact := range payload.Contacts {
+		if contact.DisplayName == "First Last" {
+			sawFirstLast = true
+		}
+		if contact.DisplayName == "" || len(contact.PhoneNumbers) != 1 {
+			t.Fatalf("bad contact = %#v", contact)
+		}
+		if contact.JID != "" || contact.Username != "" {
+			t.Fatalf("leaked source fields = %#v", contact)
+		}
+		if strings.HasPrefix(contact.DisplayName, "@") || strings.HasPrefix(contact.DisplayName, "+") || contact.DisplayName == "jid-only" {
+			t.Fatalf("unsafe display name exported: %#v", contact)
+		}
+	}
+	if !sawFirstLast {
+		t.Fatalf("missing composed first/last name: %#v", payload.Contacts)
+	}
+}
+
+func assertContactExportKeys(t *testing.T, data []byte) {
+	t.Helper()
+	var root map[string]json.RawMessage
+	if err := json.Unmarshal(data, &root); err != nil {
+		t.Fatal(err)
+	}
+	contactsJSON, ok := root["contacts"]
+	if !ok || len(root) != 1 {
+		t.Fatalf("root keys = %#v, want only contacts", root)
+	}
+	var contacts []map[string]json.RawMessage
+	if err := json.Unmarshal(contactsJSON, &contacts); err != nil {
+		t.Fatal(err)
+	}
+	for _, contact := range contacts {
+		if _, ok := contact["display_name"]; !ok {
+			t.Fatalf("contact keys = %#v, missing display_name", contact)
+		}
+		if _, ok := contact["phone_numbers"]; !ok {
+			t.Fatalf("contact keys = %#v, missing phone_numbers", contact)
+		}
+		if len(contact) != 2 {
+			t.Fatalf("contact keys = %#v, want only display_name and phone_numbers", contact)
+		}
+	}
+}
+
+func TestMetadataAdvertisesContactExport(t *testing.T) {
+	manifest := controlManifest()
+	command, ok := manifest.Commands["contact-export"]
+	if !ok {
+		t.Fatalf("commands = %#v", manifest.Commands)
+	}
+	if command.Mutates || !command.JSON {
+		t.Fatalf("contact-export command = %#v", command)
+	}
+	want := []string{"telecrawl", "--json", "contacts", "export"}
+	if !slices.Equal(command.Argv, want) {
+		t.Fatalf("argv = %#v, want %#v", command.Argv, want)
 	}
 }
 

internal/cli/control.go
@@ -19,10 +19,11 @@ func controlManifest() control.Manifest {
 	m.Capabilities = []string{"metadata", "doctor", "status", "sync", "search", "backup"}
 	m.Privacy = control.Privacy{ContainsPrivateMessages: true, ExportsSecrets: false, LocalOnlyScopes: []string{"telegram-desktop", "telegram-macos-postbox", "sqlite", "encrypted-git-backup"}}
 	m.Commands = map[string]control.Command{
-		"doctor": {Title: "Doctor", Argv: []string{"telecrawl", "--json", "doctor"}, JSON: true},
-		"status": {Title: "Status", Argv: []string{"telecrawl", "--json", "status"}, JSON: true},
-		"sync":   {Title: "Import", Argv: []string{"telecrawl", "--json", "import"}, JSON: true, Mutates: true},
-		"search": {Title: "Search", Argv: []string{"telecrawl", "--json", "search"}, JSON: true},
+		"doctor":         {Title: "Doctor", Argv: []string{"telecrawl", "--json", "doctor"}, JSON: true},
+		"status":         {Title: "Status", Argv: []string{"telecrawl", "--json", "status"}, JSON: true},
+		"sync":           {Title: "Import", Argv: []string{"telecrawl", "--json", "import"}, JSON: true, Mutates: true},
+		"search":         {Title: "Search", Argv: []string{"telecrawl", "--json", "search"}, JSON: true},
+		"contact-export": {Title: "Export contacts", Argv: []string{"telecrawl", "--json", "contacts", "export"}, JSON: true},
 	}
 	return m
 }

internal/store/export.go
@@ -79,6 +79,10 @@ func (s *Store) ListContacts(ctx context.Context, limit int) ([]Contact, error) 
 	return s.contacts(ctx, limit)
 }
 
+func (s *Store) ExportContacts(ctx context.Context) ([]Contact, error) {
+	return s.allContacts(ctx)
+}
+
 func (s *Store) allContacts(ctx context.Context) ([]Contact, error) {
 	return s.contacts(ctx, 0)
 }